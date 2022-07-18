58bb1c9d210f16b005877285c1e3b88f1f48138c
Manu <e.vodor@gmail.com>
2022-07-18 22:13:12 +0200

    #2 force api lang to english

--- src/api.js
+++ src/api.js
@@ -3,31 +3,32 @@ import { fetchJson } from './helpers'
 
 // https://wiki.guildwars2.com/wiki/API:Main
 const API_BASE_URL = 'https://api.guildwars2.com/v2/'
+const API_LANG = 'en'
 
 export const getProfessions = _.memoize(async function () {
-  return await fetchJson(API_BASE_URL + `professions?ids=all`)
+  return await fetchJson(API_BASE_URL + `professions?lang=${API_LANG}&ids=all`)
 })
 
 export const getLegends = _.memoize(async function () {
-  return await fetchJson(API_BASE_URL + `legends?ids=all`)
+  return await fetchJson(API_BASE_URL + `legends?lang=${API_LANG}&ids=all`)
 })
 
 export const getPets = _.memoize(async function () {
-  return await fetchJson(API_BASE_URL + 'pets?ids=all')
+  return await fetchJson(API_BASE_URL + `pets?lang=${API_LANG}&ids=all`)
 })
 
 export const getAmulets = _.memoize(async function () {
-  return await fetchJson(API_BASE_URL + 'pvp/amulets?ids=all')
+  return await fetchJson(API_BASE_URL + `pvp/amulets?lang=${API_LANG}&ids=all`)
 })
 
 export const getSkills = _.memoize(async function (ids) {
-  return await fetchJson(API_BASE_URL + `skills?ids=${ids.join(',')}`)
+  return await fetchJson(API_BASE_URL + `skills?lang=${API_LANG}&ids=${ids.join(',')}`)
 })
 
 export const getSpecializations = _.memoize(async function (ids) {
-  return await fetchJson(API_BASE_URL + `specializations?ids=${ids.join(',')}`)
+  return await fetchJson(API_BASE_URL + `specializations?lang=${API_LANG}&ids=${ids.join(',')}`)
 })
 
 export const getTraits = _.memoize(async function (ids) {
-  return await fetchJson(API_BASE_URL + `traits?ids=${ids.join(',')}`)
+  return await fetchJson(API_BASE_URL + `traits?lang=${API_LANG}&ids=${ids.join(',')}`)
 })
