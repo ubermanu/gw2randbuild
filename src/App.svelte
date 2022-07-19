9eadda20fc30a508cc059353196d7cac954f7658
ubermanu <manu@paca.happy-dev.fr>
2022-07-19 18:05:51 +0200

    update header

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
     import 'bulma/css/bulma.min.css'
     import 'bulma-prefers-dark/css/bulma-prefers-dark.min.css'
 
+    import logo from './assets/Guild_Wars_2_Logo_Text.svg'
     import { generate } from './main.js'
     import BuildCard from './BuildCard.svelte'
     import { writable } from 'svelte/store'
@@ -23,7 +24,9 @@
 
 <main>
     <section class="container">
-        <h1 class="title">🎲 GW2 Build Randomizer</h1>
+        <h1 class="title" aria-label="GW2 Build Randomizer"><img class="game-logo" src={logo} alt="GW2"></h1>
+        <div class="subtitle" aria-hidden="true">Build Randomizer</div>
+        <br>
         <button id="generate" class="button is-small is-primary" on:click={handleGenerate}>GENERATE</button>
         <button id="clear" class="button is-small" on:click={handleClear}>CLEAR</button>
         <label class="ml-4">
@@ -42,6 +45,21 @@
 <style>
     main {
         padding: 3rem 1em;
+    }
+
+    .game-logo {
+        max-height: 0.9em;
+        vertical-align: middle;
+        line-height: 1.2;
+    }
+
+    .title > span {
+        vertical-align: middle;
+        font-weight: normal;
+    }
+
+    .subtitle {
+        font-weight: lighter;
     }
 
     .builds {
